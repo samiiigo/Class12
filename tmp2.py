e50df8ee7aa47944933f9fab332fd45cf816424c
samiiigo <samyogg176@gmail.com>
2024-11-10 22:33:39 -0600

    init pycharm

--- tmp2.py
+++ tmp2.py
@@ -1,5 +1,33 @@
-x=[4,1]
+#!/bin/python3
 
-while x:
-    print('i')
-    x.pop()
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    
+    min, max = arr, []
+    for i in range(len(arr)):
+        arr_=arr.copy()
+        arr_.pop(i)
+        
+        if sum(min)>sum(arr_):
+            min = arr_
+        if sum(max)<sum(arr_):
+            max=arr_
+    
+    print(sum(min), sum(max))
+
+if __name__ == '__main__':
+
+    arr = [1, 2, 3, 4, 5]
+
+    miniMaxSum(arr)
